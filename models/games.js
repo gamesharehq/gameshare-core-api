0bd282fd86e8aee278e65016881c2f431a39c092
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-09-25 19:39:06 +0100

    changed release_year to from Date to Number

--- models/games.js
+++ models/games.js
@@ -5,14 +5,14 @@ let Schema = mongoose.Schema;
 
 let Games = new Schema({
     title: { type: String, required: true, trim: true },
-    release_year: { type: Date, required: true },
+    release_year: { type: Number, required: true },
     category: { type: Schema.ObjectId, ref: 'Category', required: true },
     user: { type: Schema.ObjectId, ref: 'User', required: true },
     image: [{ type: String, required: true }],
     video: { type: String },
     description: { type: String },
     status: { type: String, enum: ['Enabled', 'Disabled'], default: 'Enabled'},
-    moderated: { type: String, enum: [true, false], default: false },
+    moderated: { type: Boolean, enum: [true, false], default: false },
     mode: { type: String, enum: ['Sell', 'Exchange'], default: 'Exchange'},
     price: { type: Number, min: 1000, max: 15000 },
     date_created: { type: Date, default: Date.now},
