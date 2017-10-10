1e735015c63f2e8a09f9c9b9d9d1fad037e1b2c0
Maxwell Obi <obi.maxwell@rocketmail.com>
2017-10-10 22:48:28 +0100

    Migrated local db to cloud @ Mongo Atlas

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ let config = {};
 config.app_port = process.env.PORT || '3000';
 config.app_secret = process.env.APP_SECRET || '2max-abh4-6k10-5hjx-8gks';
 
-config.database = process.env.DB || 'mongodb://root:chameleon@localhost:27017/gameshare?authSource=admin';
+config.database = process.env.DB || 'mongodb://root:chameleon@gamesharecluster-shard-00-00-53khx.mongodb.net:27017,gamesharecluster-shard-00-01-53khx.mongodb.net:27017,gamesharecluster-shard-00-02-53khx.mongodb.net:27017/gameshare_db?ssl=true&replicaSet=GameShareCluster-shard-0&authSource=admin';
 
 config.token_expiration = 86400; //expire in 24Hrs
 
